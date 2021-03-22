d37ecb4cd90019519d4b8ede9b15c7aa8d69502f
Eddie <eddie.ch.89d@gmail.com>
2021-03-22 15:44:03 +0100

    Spring MVC

---
+++ src/main/java/se/lexicon/spring_mvc_exercises/controllers/Home.java
@@ -0,0 +1,17 @@
+package se.lexicon.spring_mvc_exercises.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class Home {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+
+}
