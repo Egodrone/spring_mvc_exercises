17c882360e15bcaaebb431d9079c243ed037ea6d
Eddie <eddie.ch.89d@gmail.com>
2021-03-22 18:29:11 +0100

    Post method

--- src/main/java/se/lexicon/spring_mvc_exercises/controllers/Home.java
+++ src/main/java/se/lexicon/spring_mvc_exercises/controllers/Home.java
@@ -3,6 +3,8 @@ package se.lexicon.spring_mvc_exercises.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,9 +15,18 @@ public class Home {
         return "index";
     }
 
+
     @GetMapping("/contact")
     public String contact() {
         return "contact";
     }
+
+
+    @PostMapping(path = "/contact")
+    public String email(@RequestParam String email) {
+        System.out.println(email);
+        return "contact";
+    }
+
 
 }
